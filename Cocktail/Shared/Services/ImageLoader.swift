9bfa1d7d0c052006e01145b832f04013ccd4dfea
Mike Thornton <thornton.michaelp@gmail.com>
2020-12-14 03:45:17 -0600

    adds loader services

--- Cocktail/Shared/Services/ImageLoader.swift
+++ Cocktail/Shared/Services/ImageLoader.swift
@@ -5,4 +5,25 @@
 //  Created by Michael Thornton on 12/14/20.
 //
 
+import Combine
 import Foundation
+
+class ImageLoader: ObservableObject {
+    var didChange = PassthroughSubject<Data, Never>()
+    var data = Data() {
+        didSet {
+            didChange.send(data)
+        }
+    }
+
+    init(forImageAt urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else { return }
+            DispatchQueue.main.async {
+                self.data = data
+            }
+        }
+        task.resume()
+    }
+}
--- Cocktail/Shared/Services/RecipeLoader.swift
+++ Cocktail/Shared/Services/RecipeLoader.swift
@@ -5,4 +5,59 @@
 //  Created by Michael Thornton on 12/14/20.
 //
 
+import Combine
 import Foundation
+
+class RecipeLoader: ObservableObject {
+//    var didChange = PassthroughSubject<Data, Never>()
+    var didChange = PassthroughSubject<[Recipe], Never>()
+//    var data = Data() {
+//        didSet {
+//            didChange.send(data)
+//        }
+//    }
+    var recipes: [Recipe] = [] {
+        didSet {
+            didChange.send(recipes)
+        }
+    }
+
+    enum RecipeRequest {
+        static let baseUrl = "https://cocktail-api.jasonhubbard.dev/api/"
+
+        case all
+        case random
+        case byFirstCharacter(String)
+
+        var urlExtension: String {
+            switch self {
+                case .all: return "drink-list"
+                case .random: return "random-drink"
+                case .byFirstCharacter(let char): return "drink-by-letter/\(char)"
+            }
+
+        }
+
+        var url: URL? {
+            URL(string: RecipeRequest.baseUrl + self.urlExtension)
+        }
+    }
+
+    init(for recipeRequest: RecipeRequest) {
+        guard let url = recipeRequest.url else { return }
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else { return }
+
+            if let recipes = try? JSONDecoder().decode([Recipe].self, from: data) {
+                DispatchQueue.main.async {
+                    self.recipes = recipes
+                }
+            } else if let recipe = try? JSONDecoder().decode(Recipe.self, from: data) {
+                DispatchQueue.main.async {
+                    self.recipes = [recipe]
+                }
+            }
+        }
+        task.resume()
+    }
+}
